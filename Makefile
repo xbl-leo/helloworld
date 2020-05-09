e18da61a57a6c29867dd4dba89ac82db9f62bc9e
xbl-leo <896058346@qq.com>
2020-05-09 16:27:54 +0800

    “等”复制3的东西

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
+interset.o: interest.c
+	gcc -c interest.c
+
+interest: interest.o
+	gcc -o interest interest.o
+
+=======
 interest: interest.o
 	gcc -o interest interest.o
 interset.o: interest.c
 	gcc -c interest.c
+>>>>>>> 4d6c11cc1109106898c3c89b6555aae020399647
 
 clean:
 	rm -f *.o *.exe
--- interest_rate.c
+++ interest_rate.c
@@ -8,8 +8,13 @@ int main(void)
 
 	printf("Input principal:");
 	scanf("%d", &principal);
+<<<<<<< HEAD
+	printf("Input interest ratel(e.g. 5.88%% is 588):");
+	scanf("%d", &interest_ratel)
+=======
 	printf("Input interest ratel(e.g. 5.88%% is 588):")
 	scanf("%d", &interest_ratel);
+>>>>>>> 4d6c11cc1109106898c3c89b6555aae020399647
 	printf("Input month number:");
 	scanf("%d", &month);
 
